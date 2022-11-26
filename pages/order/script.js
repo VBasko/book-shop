90c9aae9a5ba9e2a2451118f229c21e10aff3e48
VBasko <106957194+VBasko@users.noreply.github.com>
2022-11-26 19:39:57 +0400

    feat: add name validation

--- pages/order/script.js
+++ pages/order/script.js
@@ -11,7 +11,36 @@ cartLink.addEventListener("click", (event) => {
 
 const cart = document.getElementById("cart");
 const cartOverlay = document.getElementById("cart-overlay");
+const cartCloseBtn = document.getElementById("close-cart-btn");
 
+cartCloseBtn.addEventListener("click", () => {
+  closeCart(cart, cartOverlay);
+});
 cartOverlay.addEventListener("click", () => {
   closeCart(cart, cartOverlay);
+});
+
+const myForm = document.getElementById("myForm");
+
+const fname = myForm["name"];
+const namePattern = /[A-Za-z]{4,}[^ \d]/;
+
+fname.addEventListener("input", () => {
+  console.log(namePattern.test(fname.value));
+  if (!namePattern.test(fname.value) || fname.value.trim() === "") {
+    fname.closest(".label-input").classList.add("invalid");
+  } else {
+    fname.closest(".label-input").classList.remove("invalid");
+  }
+});
+
+const surname = myForm["surname"];
+const street = myForm["street"];
+const house = myForm["house"];
+const flat = myForm["flat"];
+const date = myForm["date"];
+
+myForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let isValid;
 });
